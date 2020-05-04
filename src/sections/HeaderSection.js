067111cd8d284dbbf092c7cdbf8ef8b658ac6ebf
Emil Karlsson <emil.karl@gmail.com>
2020-05-04 22:33:51 +0200

    Update headercopy

--- src/sections/HeaderSection.js
+++ src/sections/HeaderSection.js
@@ -4,10 +4,10 @@ import '../assets/styles/sections/header.css'
 const HeaderSection = () => (
   <div className="header">
     <div className="header__container container">
-      <h1 className="header__title">New Myriadcoin website</h1>
+      <h1 className="header__title">New Myriad website</h1>
       <p className="header__intro">
-        If the goal are met we will design, develop and release a new version of
-        Myriadcoins website.
+        If the goal are met we will design, develop and release a new website
+        for Myriad.
       </p>
     </div>
   </div>
